ca4159bfc9db32fe88f9fae18988923f336289d1
flyovo <iamseol@kakao.com>
2023-05-17 19:57:52 +0900

    Fix [] antd 컴포넌트에 locale 설정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import ko_KR from 'ant-design-vue/lib/locale-provider/ko_KR';
 </script>
 
 <template>
-  <RouterView />
+  <a-locale-provider :locale="ko_KR">
+    <RouterView />
+  </a-locale-provider>
 </template>
 
 <style scoped></style>
