2c57e74f833b00e296cc8e870154738f3af3c000
flyovo <iamseol@kakao.com>
2023-05-21 21:35:27 +0900

    Fix [] 게이지 차트 추가 (half doughnut)

---
+++ src/components/Dashboard/HalfDoughnutChart.vue
@@ -0,0 +1,58 @@
+<template>
+  <DoughnutChart :chartData="chartData" :options="chartOptions" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
+export default defineComponent({
+  name: 'HalfDoughnutChart',
+  props: {
+    color: { type: String, required: false, default: '#78bcee' }
+  },
+  components: { DoughnutChart },
+  setup(props) {
+    const data = [30, 40, 17];
+    const chartData = {
+      labels: [
+        '1층 원무과 수납 키오스크',
+        '2층 채혈실 수납 키오스크',
+        '1층 제증명'
+      ],
+      datasets: [
+        {
+          data,
+          backgroundColor: function (context: any) {
+            if (context.dataIndex === data.length - 1) {
+              return '#e8edf0';
+            }
+            return props.color;
+          },
+          borderWidth: 0,
+          borderSkipped: 'end'
+        }
+      ]
+    };
+
+    const chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+      rotation: -90,
+      circumference: 180,
+      plugins: {
+        legend: {
+          position: 'bottom',
+          align: 'left',
+          fullSize: true
+        }
+      }
+    };
+
+    return { chartData, chartOptions };
+  }
+});
+</script>
