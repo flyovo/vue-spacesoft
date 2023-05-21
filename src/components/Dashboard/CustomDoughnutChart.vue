62f19dd964938fd05f0e0537ed74c96c039d6ae2
flyovo <iamseol@kakao.com>
2023-05-21 21:33:20 +0900

    Fix [] 도넛차트 수정

--- src/components/Dashboard/CustomDoughnutChart.vue
+++ src/components/Dashboard/CustomDoughnutChart.vue
@@ -1,72 +1,5 @@
-<!-- <template>
-  <DoughnutChart v-bind="doughnutChartProps" />
-</template>
-
-<script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
-import { DoughnutChart, useDoughnutChart } from 'vue-chart-3';
-import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
-
-Chart.register(...registerables);
-export default defineComponent({
-  name: 'DoughnutChart',
-  components: { DoughnutChart },
-  setup() {
-    const dataValues = ref([30, 40, 60, 70, 5]);
-    const dataLabels = ref(['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre']);
-    const toggleLegend = ref(true);
-
-    const testData = computed<ChartData<'doughnut'>>(() => ({
-      labels: dataLabels.value,
-      datasets: [
-        {
-          data: dataValues.value,
-          backgroundColor: [
-            '#77CEFF',
-            '#0079AF',
-            '#123E6B',
-            '#97B0C4',
-            '#A5C8ED'
-          ]
-        }
-      ]
-    }));
-
-    const options = computed<ChartOptions<'doughnut'>>(() => ({
-      scales: {
-        myScale: {
-          type: 'logarithmic',
-          position: toggleLegend.value ? 'left' : 'right'
-        }
-      },
-      plugins: {
-        legend: {
-          position: toggleLegend.value ? 'top' : 'bottom'
-        },
-        title: {
-          display: true,
-          text: 'Chart.js Doughnut Chart'
-        }
-      }
-    }));
-
-    const { doughnutChartProps, doughnutChartRef } = useDoughnutChart({
-      chartData: testData,
-      options
-    });
-
-    return {
-      // testData,
-      // options,
-      doughnutChartRef,
-      doughnutChartProps
-    };
-  }
-});
-</script> -->
-
 <template>
-  <DoughnutChart :chartData="testData" />
+  <DoughnutChart :chartData="chartData" />
 </template>
 
 <script lang="ts">
@@ -80,7 +13,7 @@ export default defineComponent({
   name: 'CustomDoughnutChart',
   components: { DoughnutChart },
   setup() {
-    const testData = {
+    const chartData = {
       labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
       datasets: [
         {
@@ -96,7 +29,7 @@ export default defineComponent({
       ]
     };
 
-    return { testData };
+    return { chartData };
   }
 });
 </script>
