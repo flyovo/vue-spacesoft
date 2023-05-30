50974e08d64bfc7ee0977e1918257fb54495db90
flyovo <iamseol@kakao.com>
2023-05-30 23:59:10 +0900

    Fix [] config 설정 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -17,5 +17,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    node: true,
+    browser: true,
+    es2020: true
   }
-}
+};
